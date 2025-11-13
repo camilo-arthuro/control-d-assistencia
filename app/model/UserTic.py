a2871a0cf79e1b175d0216eea9f54f26888b84ad
Hanna <hnnzpa@email.com>
2025-11-13 17:05:39 +0100

    modificada la tabla

--- app/model/UserTic.py
+++ app/model/UserTic.py
@@ -6,7 +6,7 @@ class RolEnum(str, Enum):
     profesor = "profesor"
     personal_servicio = "personal_servicio"
 
-class Persona(SQLModel, table=True):
+class Trabajador(SQLModel, table=True):
     id_persona: str = Field(default=None, primary_key=True)
     nombre: str = Field(max_length=100)
     apellidos: str = Field(max_length=100)
@@ -14,11 +14,16 @@ class Persona(SQLModel, table=True):
     contrasenya_correo: str = Field(max_length=100)
     rol: RolEnum
 
-class Alumno(SQLModel, table=True):
-    id_alumno: str = Field(foreign_key="persona.id_persona", primary_key=True)
-
 class PersonalServicio(SQLModel, table=True):
     id_personal: str = Field(foreign_key="persona.id_persona", primary_key=True)
 
 class Profesor(SQLModel, table=True):
     id_profesor: str = Field(foreign_key="persona.id_persona", primary_key=True)
+    
+class Alumno(SQLModel, table=True):
+    id_alumno: str = Field(default=None, primary_key=True)
+    nombre: str = Field(max_length=100)
+    apellidos: str = Field(max_length=100)
+    correoelectronico: str = Field(max_length=100, unique=True)
+    contrasenya_correo: str = Field(max_length=100)
+    rol: RolEnum
